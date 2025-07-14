06985745812b412ebd1a5d844cc739110aaae6ee
RuslanSalbiev <salbiev.ru@mail.ru>
2025-07-14 15:25:29 +0400

    пагинация

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -5,11 +5,13 @@ const API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
 
 
-export const getNews =async ()=> {
+export const getNews =async (page_number = 1, page_size = 10)=> {
     try {
       const response = await axios.get('/api',{
         params:{ 
-            apiKey : API_KEY
+            apiKey : API_KEY,
+            page_size,
+            page_number
       }})
       return response.data 
     } catch (e) {
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,15 +5,19 @@ import NewsBanner from '../../component/newsBanner/newsBanner.jsx'
 import { getNews } from '../../api/apiNews.js'
 import NewsList from '../../component/NewsList/newsList.jsx'
 import Skeleton from '../../component/Skeleton/Skeleton.jsx'
+import Pagination from '../../api/Pagination/Pagination.jsx'
 
 const Main = () => {
 const [news, setNews] = useState([])
 const [isLoading, setisLoading] = useState(true)
-    useEffect(() => {
-     const fetchNews = async()=>{
+const [currentPage, setcurrentPage] = useState(1)
+const totalPage = 10
+const pagesize = 10
+
+ const fetchNews = async(currentPage)=>{
         try {
            setisLoading(true)
-       const res = await getNews ()
+       const res = await getNews (currentPage,pagesize)
 
        setNews(res.news);
        setisLoading(false)
@@ -22,16 +26,20 @@ const [isLoading, setisLoading] = useState(true)
         
      } 
      }
+    useEffect(() => {
     
-     fetchNews() 
-    }, [])
+    
+     fetchNews(currentPage) 
+    }, [currentPage])
 
     
     
   return (
     <main className={styles.main}>
       {news.length > 0 && !isLoading ?(<NewsBanner item = {news[0]}/>):(<Skeleton count={1} type={'banner'}/>)}
+      <Pagination totalPage = {totalPage} currentPage={currentPage} setcurrentPage={setcurrentPage}/>
       {!isLoading? <NewsList news = {news}/> : <Skeleton count={10} type={'item'}/>}
+      <Pagination totalPage = {totalPage} currentPage={currentPage} setcurrentPage={setcurrentPage}/>
       
       </main>
   )
